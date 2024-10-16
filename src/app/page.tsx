fbffee3523b82f489e8d8c974403d1c23e12a589
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2024-10-16 10:25:03 +0000

    153

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 import { UserButton } from "@/features/auth/components/user-button";
-import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
   return (
     <div>
-      loged In!
       <UserButton />
     </div>
   );
--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useAuthActions } from "@convex-dev/auth/react";
 import {
     Avatar,
     AvatarFallback,
@@ -12,10 +12,11 @@ import {
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import { useCurrentUser } from "../hooks/use-current-user";
-import { Loader } from "lucide-react";
+import { Loader, LogOut } from "lucide-react";
 
 export const UserButton = () => {
 
+    const { signOut } = useAuthActions();
     const { data, isLooding } = useCurrentUser();
 
     if(isLooding){
@@ -25,23 +26,24 @@ export const UserButton = () => {
         return null;
     }
 
-    const { image, name, email } = data;
+    const { image, name } = data;
 
     const avatarFallback = name!.charAt(0).toUpperCase();
-    
+
     return (
         <DropdownMenu modal={false}>
             <DropdownMenuTrigger className="outline-none relative">
                 <Avatar className="size-10 hover:opacity/75 transition">
-                    <AvatarImage />
-                    <AvatarFallback>
-
+                    <AvatarImage alt={name} src={image} />
+                    <AvatarFallback className="bg-sky-500 text-white">
+                        {avatarFallback}
                     </AvatarFallback>
                 </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="center" side="right" className="w-60">
-                <DropdownMenuItem>
-                    
+                <DropdownMenuItem onClick={ () => signOut() } className="h-10">
+                    <LogOut className="size-4 mr-2"/>
+                    Log out
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
