0eb77fd62530919b6580905bf7525bd18489fed9
Shujaat Khan <148489139+shujaatkhanjadoon@users.noreply.github.com>
2024-10-16 11:56:22 +0000

    2.14

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -8,6 +8,8 @@ import {
     DialogTitle
 } from "@/components/ui/dialog";
 import { useCreateWorkspaceModal } from "../store/use-create-wordspace-modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const CreateWorkspaceModal = () => {
     const [open, setOpen] = useCreateWorkspaceModal();
@@ -24,7 +26,21 @@ export const CreateWorkspaceModal = () => {
                     </DialogTitle>
                 </DialogHeader>
                 <DialogDescription>
-
+                    <form className="space-y-4">
+                        <Input 
+                        value=""
+                        disabled={false}
+                        required
+                        autoFocus
+                        minLength={3}
+                        placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+                        />
+                        <div className="flex justify-end">
+                            <Button disabled={false} >
+                                Create
+                            </Button>
+                        </div>
+                    </form>
                 </DialogDescription>
             </DialogContent>
         </Dialog>
